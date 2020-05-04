2f2ddb537e06bc90d54d872c2f58d7ceebdbfa20
Lukas Romsicki <lukas@romsicki.com>
2020-05-03 20:48:45 -0400

    Updates

---
+++ precache-manifest.b94f379eaa999a86f5a994a07a83ebdd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942b7b3a5d9a01273c5b5a7f2347335d",
+    "url": "/clouds-website/index.html"
+  },
+  {
+    "revision": "666fe1c416e8d01b4ac4",
+    "url": "/clouds-website/static/css/2.10f67ca9.chunk.css"
+  },
+  {
+    "revision": "d58579293f3b7776c7a3",
+    "url": "/clouds-website/static/css/main.74019b7b.chunk.css"
+  },
+  {
+    "revision": "666fe1c416e8d01b4ac4",
+    "url": "/clouds-website/static/js/2.b34673bd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/clouds-website/static/js/2.b34673bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d58579293f3b7776c7a3",
+    "url": "/clouds-website/static/js/main.db082e8f.chunk.js"
+  },
+  {
+    "revision": "9d4fb4a21160c937eb4c",
+    "url": "/clouds-website/static/js/runtime-main.6a6e2656.js"
+  },
+  {
+    "revision": "c6f7f063b6cf9fc6f82e9feda6b4ea47",
+    "url": "/clouds-website/static/media/SF-Pro-Display-Heavy.c6f7f063.otf"
+  },
+  {
+    "revision": "e570ec818d03840bfd5072c83deb1b21",
+    "url": "/clouds-website/static/media/SF-Pro-Display-Semibold.e570ec81.otf"
+  },
+  {
+    "revision": "b21791d484d0ee056d789bd7910d1383",
+    "url": "/clouds-website/static/media/SF-Pro-Text-Medium.b21791d4.otf"
+  },
+  {
+    "revision": "7200d5816a0ef2ffb61b8085b6646dfb",
+    "url": "/clouds-website/static/media/SF-Pro-Text-MediumItalic.7200d581.otf"
+  },
+  {
+    "revision": "7080653994709ec78daac2d4cd4acf8d",
+    "url": "/clouds-website/static/media/app_store.70806539.svg"
+  },
+  {
+    "revision": "8e4773c08e118bfb0c4a40aa083a89f6",
+    "url": "/clouds-website/static/media/screenshots.8e4773c0.png"
+  }
+]);
